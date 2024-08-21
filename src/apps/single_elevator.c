37a167f8c12f0f00cfcc102c35c55cc399f0e62d
Einstein <conradoeinsteinpro@gmail.com>
2024-08-21 13:54:08 -0300

    ### Added  - Struct "pessoas" and struct "elevadores".  - Some function prototypes.

--- src/apps/single_elevator.c
+++ src/apps/single_elevator.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#define CAPACIDADE 10
+
+typedef struct pessoas {
+    char nome[31];
+    int andarInicial;
+    int andarFinal;
+}pessoa;
+
+typedef struct elevadores {
+    pessoa *pessoa;
+    int capacidade = CAPACIDADE;
+    struct elevadores *prox;
+    struct elevadores *ant;
+}elevador;
+
+int embarcar(elevador **elevador, pessoa pessoa);
+int desembarcar(elevador **elevador);
+void listar_passageiros(elevador **elevador);
 
 int main(void) {
     printf("Hello, World!\n");
